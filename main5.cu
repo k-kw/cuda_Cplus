7e6f8d0a36db3b02be5640a0e84d02520545af33
kwmt <kouki1212@chiba-u.jp>
2022-03-18 02:20:45 +0900

    kaku OK but slow. i will compute by float

--- main5.cu
+++ main5.cu
@@ -108,18 +108,6 @@ __global__ void pad(double* out, int x, int y, double* in)
 }
 
 
-
-//__global__ void H(cufftComplex* H, double lam, double z, double d, int x, int y) {
-//	double u = 1 / ((double)x * d), v = 1 / ((double)y * d);
-//
-//	int idx = blockDim.x * blockIdx.x + threadIdx.x;
-//	int idy = blockDim.y * blockIdx.y + threadIdx.y;
-//	
-//	H[idx] = make_cuComplex((float)Re[i], (float)Im[i]);
-//}
-
-
-
 //角スペクトル法のHを直接計算する関数
 void H_kaku(My_ComArray_2D* H, double lam, double z, double d, int x, int y) {
 
@@ -187,13 +175,12 @@ void Opad(double* img_out, int x, int y, double* img_in) {
 void fft_2D_cuda_dev(int x, int y, cufftComplex* dev)
 {
 	cufftHandle plan;
+	//doubleのときはZ2Z?
 	cufftPlan2d(&plan, x, y, CUFFT_C2C);
 	cufftExecC2C(plan, dev, dev, CUFFT_FORWARD);
 	cufftDestroy(plan);
 
-	/*for (int i = 0; i < x * y; i++) {
-		dev[i] = make_cuComplex((float)(cuCrealf(dev[i]) / (x * y)), (float)(cuCimagf(dev[i]) / (x * y)));
-	}*/
+	
 }
 
 
@@ -213,24 +200,7 @@ void cufftcom_to_mycom(My_ComArray_2D* out, cufftComplex* in, int s) {
 	}
 }
 
-//複素数配列乗算関数
-void mul_com(int size, My_ComArray_2D* in1, My_ComArray_2D* in2, My_ComArray_2D* out) {
-	double* Retmp, * Imtmp;
-	Retmp = new double[size];
-	Imtmp = new double[size];
 
-	for (int i = 0; i < size; i++) {
-		Retmp[i] = in1->Re[i] * in2->Re[i] - in1->Im[i] * in2->Im[i];
-		Imtmp[i] = in1->Re[i] * in2->Im[i] + in1->Im[i] * in2->Re[i];
-	}
-
-	for (int i = 0; i < size; i++) {
-		out->Re[i] = Retmp[i];
-		out->Im[i] = Imtmp[i];
-	}
-	delete[]Retmp;
-	delete[]Imtmp;
-};
 
 void elim(My_ComArray_2D* in, int ix, int iy, My_ComArray_2D* out) {
 	int x, y;
@@ -250,15 +220,8 @@ void elim(My_ComArray_2D* in, int ix, int iy, My_ComArray_2D* out) {
 
 __global__ void Hcuda(double* Re, double* Im, int x, int y, double u, double v, double z, double lam)
 {
-	
 
-	////H計算
-	//for (int i = 0; i < y; i++) {
-	//	for (int j = 0; j < x; j++) {
-	//		H->Re[i * x + j] = cos(2 * M_PI * z * sqrt((1 / lam) * (1 / lam) - (u * ((double)j - x / 2)) * (u * ((double)j - x / 2)) - (v * ((double)i - y / 2)) * (v * ((double)i - y / 2))));
-	//		H->Im[i * x + j] = sin(2 * M_PI * z * sqrt((1 / lam) * (1 / lam) - (u * ((double)j - x / 2)) * (u * ((double)j - x / 2)) - (v * ((double)i - y / 2)) * (v * ((double)i - y / 2))));
-	//	}
-	//}
+
 
 	int idx = blockDim.x * blockIdx.x + threadIdx.x;
 	int idy = blockDim.y * blockIdx.y + threadIdx.y;
@@ -271,35 +234,7 @@ __global__ void Hcuda(double* Re, double* Im, int x, int y, double u, double v, 
 
 }
 
-//
-//__global__ void Hcuda(double* Re, double* Im, double d, double lam, double z, int x, int y)
-//{
-//	double u = 1 / ((double)x * d), v = 1 / ((double)y * d);
-//
-//
-//	int idx = blockDim.x * blockIdx.x + threadIdx.x;
-//	int idy = blockDim.y * blockIdx.y + threadIdx.y;
-//
-//	if (idy < y && idx < x) {
-//		Re[idy * x + idx] = (double)3.14;
-//		Im[idy * x + idx] = (double)3.14;
-//
-//	}
-//}
-//
-//__global__ void Hcuda2(double* Re, double* Im, double d, double lam, double z, int x, int y, double value)
-//{
-//	//double u = 1 / ((double)x * d), v = 1 / ((double)y * d);
-//
-//
-//	int idx = blockDim.x * blockIdx.x + threadIdx.x;
-//	int idy = blockDim.y * blockIdx.y + threadIdx.y;
-//	if (idy < y && idx < x) {
-//		Re[idy * x + idx] = value;
-//		Im[idy * x + idx] = value;
-//	}
-//}
-//
+
 
 __global__ void  shift(double* ore, double* oim, double* re, double* im, int x, int y)
 {
@@ -326,60 +261,10 @@ __global__ void  shift(double* ore, double* oim, double* re, double* im, int x, 
 	}
 }
 
-//void shift(cufftComplex* out, My_ComArray_2D* in, int x, int y)
-//{
-//
-//	float tmpRe, tmpIm;
-//
-//	for (int i = 0; i < y; i++) {
-//		for (int j = 0; j < x; j++) {
-//
-//			if (j < x / 2 && i < y / 2) {
-//
-//				tmpRe = (float)in->Re[(i + y / 2) * x + (j + x / 2)];
-//				tmpIm = (float)in->Im[(i + y / 2) * x + (j + x / 2)];
-//				out[i * x + j] = make_cuComplex(tmpRe, tmpIm);
-//
-//				tmpRe = (float)in->Re[i * x + j];
-//				tmpIm = (float)in->Im[i * x + j];
-//				out[(i + y / 2) * x + (j + x / 2)] = make_cuComplex(tmpRe, tmpIm);
-//			}
-//
-//			else if (j >= x / 2 && i < y / 2) {
-//
-//				tmpRe = (float)in->Re[(i + y / 2) * x + (j - x / 2)];
-//				tmpIm = (float)in->Im[(i + y / 2) * x + (j - x / 2)];
-//				out[i * x + j] = make_cuComplex(tmpRe, tmpIm);
-//
-//				tmpRe = (float)in->Re[i * x + j];
-//				tmpIm = (float)in->Im[i * x + j];
-//				out[(i + y / 2) * x + (j + x / 2)] = make_cuComplex(tmpRe, tmpIm);
-//			}
-//		}
-//	}
-//}
 
 
 
-//__global__ void mul_cucom(cufftComplex* out, cufftComplex* in1, cufftComplex* in2, int s)
-//{
-//	//int idx = blockDim.x * blockIdx.x + threadIdx.x;
-//	float tmpRe, tmpIm;
-//	/*if (idx < s) {
-//		tmpRe = cuCrealf(in1[idx]) * cuCrealf(in2[idx]) - cuCimagf(in1[idx]) * cuCimagf(in2[idx]);
-//		tmpIm = cuCrealf(in1[idx]) * cuCimagf(in2[idx]) + cuCimagf(in1[idx]) * cuCrealf(in2[idx]);
-//
-//		out[idx] = make_cuComplex(tmpRe, tmpIm);
-//
-//	}*/
-//
-//	for (int i = 0; i < s; i++) {
-//		tmpRe = (cuCrealf(in1[i]) * cuCrealf(in2[i])) - (cuCimagf(in1[i]) * cuCimagf(in2[i]));
-//		tmpIm = (cuCrealf(in1[i]) * cuCimagf(in2[i])) + (cuCimagf(in1[i]) * cuCrealf(in2[i]));
-//
-//		out[i] = make_cuComplex(tmpRe, tmpIm);
-//	}
-//}
+
 
 __global__ void mulcom(double* ore, double* oim, double* re, double* im, double* re2, double* im2, int s)
 {
@@ -412,7 +297,7 @@ void kaku(My_ComArray_2D* in, int x, int y, double lamda, double d, double z)
 	fft_2D_cuda_dev(2 * x, 2 * y, dev);
 	cudaMemcpy(host, dev, sizeof(cufftComplex) * x * y * 4, cudaMemcpyDeviceToHost);
 	cufftcom_to_mycom(tmp, host, 4 * x * y);
-	
+
 	double* re;
 	cudaMalloc((void**)&re, sizeof(double) * x * y * 4);
 	cudaMemcpy(re, tmp->Re, sizeof(double) * x * y * 4, cudaMemcpyHostToDevice);
@@ -434,30 +319,8 @@ void kaku(My_ComArray_2D* in, int x, int y, double lamda, double d, double z)
 
 	double u = 1 / ((double)2 * SX * d), v = 1 / ((double)2 * SY * d);
 	dim3 grid(32, 32), block(32, 32);
-	Hcuda<<<grid, block>>>(ReH, ImH, 2 * SX, 2 * SY, u, v, z, lamda);
-	shift<<<grid, block>>>(ReHs, ImHs, ReH, ImH, 2 * x, 2 * y);
-
-
-
-	////デバッグ
-	//cudaMemcpy(tmp->Re, ReHs, sizeof(double) * x * y * 4, cudaMemcpyDeviceToHost);
-	//cout << "CUDA\t" << tmp->Re[0] << "\t" << tmp->Re[4 * SX * SY - 1] << endl;
-	//My_ComArray_2D* th;
-	//th = new My_ComArray_2D(4 * x * y, 2 * x, 2 * y);
-	////H_kaku(th, lamda, z, d, 2 * x, 2 * y);
-	//th->H_kaku(lamda, z, d);
-	//cout << "contemporary\t" << th->Re[0] << "\t" << th->Re[4 * SX * SY - 1] << endl;
-
-
-	//H_kaku(H, lamda, z, d, 2 * x, 2 * y);
-	////H->H_kaku(lamda, z, d);
-
-	/*double* re2;
-	cudaMalloc((void**)&re2, sizeof(double) * x * y * 4);
-	cudaMemcpy(re2, H->Re, sizeof(double) * x * y * 4, cudaMemcpyHostToDevice);
-	double* im2;
-	cudaMalloc((void**)&im2, sizeof(double) * x * y * 4);
-	cudaMemcpy(im2, H->Im, sizeof(double) * x * y * 4, cudaMemcpyHostToDevice);*/
+	Hcuda << <grid, block >> > (ReH, ImH, 2 * SX, 2 * SY, u, v, z, lamda);
+	shift << <grid, block >> > (ReHs, ImHs, ReH, ImH, 2 * x, 2 * y);
 
 
 	//掛け算
@@ -465,9 +328,9 @@ void kaku(My_ComArray_2D* in, int x, int y, double lamda, double d, double z)
 	cudaMalloc((void**)&re3, sizeof(double) * x * y * 4);
 	double* im3;
 	cudaMalloc((void**)&im3, sizeof(double) * x * y * 4);
-	
+
 	//dim3 grid(128, 128), block(128, 128);
-	mulcom<<<1024,1024>>>(re3, im3, re, im, ReHs, ImHs, x * y * 4);
+	mulcom << <1024, 1024 >> > (re3, im3, re, im, ReHs, ImHs, x * y * 4);
 
 	cudaMemcpy(tmp->Re, re3, sizeof(double) * x * y * 4, cudaMemcpyDeviceToHost);
 	cudaMemcpy(tmp->Im, im3, sizeof(double) * x * y * 4, cudaMemcpyDeviceToHost);
@@ -498,26 +361,121 @@ void kaku(My_ComArray_2D* in, int x, int y, double lamda, double d, double z)
 	cudaFree(re3);
 	cudaFree(im3);
 
-	
+
 	delete tmp;
 
 }
 
+//__global__ void set_cufftDoubleComplex(cufftDoubleComplex* out, double* Re, double* Im, int s) {
+//
+//	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+//
+//	if (idx < s) {
+//		out[idx] = make_cuDoubleComplex(Re[idx], Im[idx]);
+//	}
+//}
+//
+//void cufftdoublecom_to_mycom(My_ComArray_2D* out, cufftDoubleComplex* in, int s) {
+//
+//	for (int i = 0; i < s; i++) {
+//		out->Re[i] = cuCreal(in[i]);
+//		out->Im[i] = cuCimag(in[i]);
+//
+//	}
+//}
 
-void kakucuda(My_ComArray_2D* in, int x, int y, double lamda, double d, double z) {
+__global__ void Hcudaf(float* Re, float* Im, int x, int y, float u, float v, float z, float lam)
+{
+
+
+
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	int idy = blockDim.y * blockIdx.y + threadIdx.y;
+
+	if (idy < y && idx < x) {
+		Re[idy * x + idx] = cos(2 * M_PI * z * sqrt(sqr(1 / lam) - sqr(u * ((float)idx - x / 2)) - sqr(v * ((float)idy - y / 2))));
+		Im[idy * x + idx] = sin(2 * M_PI * z * sqrt(sqr(1 / lam) - sqr(u * ((float)idx - x / 2)) - sqr(v * ((float)idy - y / 2))));
+	}
+
+
+}
+
+__global__ void  shiftf(float* ore, float* oim, float* re, float* im, int x, int y)
+{
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	int idy = blockDim.y * blockIdx.y + threadIdx.y;
+
+	if (idy < y && idx < x) {
+
+		if (idx < x / 2 && idy < y / 2) {
+			ore[idy * x + idx] = re[(idy + y / 2) * x + (idx + x / 2)];
+			ore[(idy + y / 2) * x + (idx + x / 2)] = re[idy * x + idx];
+			oim[idy * x + idx] = im[(idy + y / 2) * x + (idx + x / 2)];
+			oim[(idy + y / 2) * x + (idx + x / 2)] = im[idy * x + idx];
+		}
+		else if (idx >= x / 2 && idy < y / 2) {
+			ore[idy * x + idx] = re[(idy + y / 2) * x + (idx - x / 2)];
+			ore[(idy + y / 2) * x + (idx - x / 2)] = re[idy * x + idx];
+			oim[idy * x + idx] = im[(idy + y / 2) * x + (idx - x / 2)];
+			oim[(idy + y / 2) * x + (idx - x / 2)] = im[idy * x + idx];
+		}
+
+
+
+	}
+}
+
+//floatXcufftCom
+__global__ void mulcomcufftcom(cufftComplex* out, float* re, float* im, cufftComplex* in, int s)
+{
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if (idx < s) {
+		ore[idx] = re[idx] * re2[idx] - im[idx] * im2[idx];
+		oim[idx] = re[idx] * im2[idx] + im[idx] * re2[idx];
+
+	}
+}
+
+void kakucuda(My_ComArray_2D* in, int x, int y, float lamda, float d, float z) {
 
 	My_ComArray_2D* tmp;
 	tmp = new My_ComArray_2D(4 * x * y, 2 * x, 2 * y);
 	in->zeropad(tmp);
 
-	cufftDoubleComplex* host;
-	host = (cufftDoubleComplex*)malloc(sizeof(cufftDoubleComplex) * x * y * 4);
-	set_cufftdoublecomplex(host, tmp->Re, tmp->Im, x * y * 4);
+	cufftComplex* host;
+	host = (cufftComplex*)malloc(sizeof(cufftComplex) * x * y * 4);
+	set_cufftcomplex(host, tmp->Re, tmp->Im, x * y * 4);
 
 
 	cufftComplex* dev;
 	cudaMalloc((void**)&dev, sizeof(cufftComplex) * x * y * 4);
 	cudaMemcpy(dev, host, sizeof(cufftComplex) * x * y * 4, cudaMemcpyHostToDevice);
+
+
+	fft_2D_cuda_dev(2 * x, 2 * y, dev);
+
+	double* ReH, * ImH;
+	cudaMalloc((void**)&ReH, sizeof(float) * x * y * 4);
+	cudaMalloc((void**)&ImH, sizeof(float) * x * y * 4);
+
+	double* ReHs, * ImHs;
+	cudaMalloc((void**)&ReHs, sizeof(float) * x * y * 4);
+	cudaMalloc((void**)&ImHs, sizeof(float) * x * y * 4);
+
+	double u = 1 / ((float)2 * SX * d), v = 1 / ((float)2 * SY * d);
+	dim3 grid(32, 32), block(32, 32);
+	Hcudaf << <grid, block >> > (ReH, ImH, 2 * SX, 2 * SY, u, v, z, lamda);
+	shiftf << <grid, block >> > (ReHs, ImHs, ReH, ImH, 2 * x, 2 * y);
+
+
+	//掛け算
+	float* rsre;
+	cudaMalloc((void**)&rsre, sizeof(float) * x * y * 4);
+	float* rsim;
+	cudaMalloc((void**)&rsim, sizeof(float) * x * y * 4);
+
+
 
 }
 
@@ -548,26 +506,9 @@ int main(void) {
 	img2->data_to_ucimg(com->Re);
 	img2->img_write(ompath);
 
-
-
-
-
-	//My_ComArray_2D* tmp;
-	//tmp = new My_ComArray_2D(4 * SX * SY, 2 * SX, 2 * SY);
-	////デバッグ
-	//double* ReH, * ImH;
-	//cudaMalloc((void**)&ReH, sizeof(double) * SX * SY * 4);
-	//cudaMalloc((void**)&ImH, sizeof(double) * SX * SY * 4);
-
-
-	//dim3 grid(32, 32), block(32, 32);
-	//Hcuda2<<<grid, block>>>(ReH, ImH, 1.496e-05, 532e-09, 0.1, 2 * SX, 2 * SY, 3.14);
-
-
-	//cudaMemcpy(tmp->Re, ReH, sizeof(double) * SX * SY * 4, cudaMemcpyDeviceToHost);
-	//cout << tmp->Re[SX * SY * 4 - 1] << "\t" << tmp->Re[1] << endl;
-
-
+	//デバッグ
+	img->ucimg_to_double(com->Re);
+	kakucuda(com, SX, SY, 532e-09, 1.496e-05, 0.1);
 
 
 
